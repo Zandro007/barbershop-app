61395f985b24b0a73ae573c9ce8a59a1015f7930
Eli Soares <zandro007@gmail.com>
2023-12-05 11:36:47 -0300

    Add files via upload

---
+++ barbershop-app/src/Controller/LoginController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Controller.Helper.LoginHelper;
+import Model.Usuario;
+import View.Login;
+
+/**
+ *
+ * @author tiago
+ */
+public class LoginController {
+
+    private final Login view;
+    private LoginHelper helper;
+
+    public LoginController(Login view) {
+        this.view = view;
+        this.helper = new LoginHelper(view);
+    }
+    
+    
+    
+    
+    
+    public void entrarNoSistema(){
+    
+        //Pegar um  Usuario da View
+        Usuario usuario = helper.obterModelo();
+        
+       
+       //Pesquisa Usuario no Banco
+       ////Se o usuario da view tiver mesmo usuario e senha que o usuario vindo do banco direcionar para menu principal
+       //Senão mostrar uma mensagem ao usuario "Usuario ou senha invalidos"
+    }
+    
+    
+    public void fizTarefa(){
+        System.out.println("Busquei algo do banco de dados");
+        
+        this.view.exibeMensagem("Executei o fiz tarefa");
+    }
+    
+}
